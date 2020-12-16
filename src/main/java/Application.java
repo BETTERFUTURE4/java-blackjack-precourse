a68b999b5a397b1e59f6fe8b536922c7158b58ce
KJunseo <max9106@naver.com>
2020-12-17 04:41:25 +0900

    feat: UI담당 클래스 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,11 @@
+import view.InputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        InputView inputView = new InputView(scanner);
     }
 }
