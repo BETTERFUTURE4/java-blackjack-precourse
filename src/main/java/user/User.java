bfc2085f80e7e2115c89d131c62d48fd31814927
KJunseo <max9106@naver.com>
2020-12-18 01:12:21 +0900

    feat: 카드의 합을 구하는 기능

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
+    private static final int BLACK_JACK = 21;
     private static final String COMMA = ", ";
 
     private final List<Card> cards = new ArrayList<>();
+    private int score = 0;
 
     public void addCard(Card card) {
         cards.add(card);
+        score += card.score();
     }
 
     public List<Card> getCards() {
@@ -24,5 +27,9 @@ public class User {
             allCardInformation.add(card.getInformation());
         }
         return String.join(COMMA, allCardInformation);
+    }
+
+    public int getScore() {
+        return score;
     }
 }
